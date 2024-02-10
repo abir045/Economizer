2e8fd53fb5b0929e738e7e989c23215d2c3cf99d
unknown <abir045@gmail.com>
2024-02-10 14:27:42 +0600

    updated navbar

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -6,15 +6,42 @@ type Props = {};
 
 const Navbar = (props: Props) => {
   const [selected, setSelected] = useState("dashboard");
-  return (
-    <div className="flex justify-between items-center mb-[0.25rem] py-[0.5rem] px-0">
-      {/* left side */}
-      <div className="inline-flex items-center  gap-[0.75rem]">
-        <PixIcon className="flex" />
 
-        <h4 className="uppercase">Economizer</h4>
+  return (
+    <div className="flex justify-between items-center mb-[0.25rem] py-[0.5rem] px-0 text-gray-300 ">
+      {/* left side */}
+
+      <div className="inline-flex items-center  gap-[0.75rem]">
+        <PixIcon className="text-[28px]" />
+
+        <h4 className="text-base">Economizer</h4>
       </div>
       {/* right side */}
+
+      <div className="flex justify-between items-center gap-[2rem]">
+        <div className="hover:text-primary-100 ">
+          <Link
+            to="/"
+            onClick={() => setSelected("dashboard")}
+            className={`${
+              selected === "dashboard" ? "text-primary-100" : "text-gray-700"
+            }`}
+          >
+            dashboard
+          </Link>
+        </div>
+        <div className="hover:text-primary-100 ">
+          <Link
+            to="/predictions"
+            onClick={() => setSelected("predictions")}
+            className={`${
+              selected === "predictions" ? "text-primary-100" : "text-gray-700"
+            }`}
+          >
+            predictions
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+
+// eslint-disable-next-line no-undef
 module.exports = {
   darkMode: ["class"],
   content: [
@@ -18,39 +20,50 @@ module.exports = {
     },
     extend: {
       colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
+          // light green
+          100: "#d0fcf4",
+          200: "#a0f9e9",
+          300: "#71f5de",
+          400: "#41f2d3",
+          500: "#12efc8",
+          600: "#0ebfa0",
+          700: "#0b8f78",
+          800: "#076050",
+          900: "#043028",
         },
         secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
+          // yellow
+          100: "#fcf0dd",
+          200: "#fae1bb",
+          300: "#f7d299",
+          400: "#f5c377",
+          500: "#f2b455",
+          600: "#c29044",
+          700: "#916c33",
+          800: "#614822",
+          900: "#302411",
         },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
+        tertiary: {
+          // purple
+          500: "#8884d8",
         },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
+        background: {
+          light: "#2d2d34",
+          main: "#1f2026",
         },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+      fontSize: {
+        "2xl": "1.5rem",
+        "3xl": "2rem",
+      },
+      fontWeight: {
+        normal: 400,
+        medium: 500,
+        bold: 700,
       },
       borderRadius: {
         lg: "var(--radius)",
@@ -73,5 +86,6 @@ module.exports = {
       },
     },
   },
+  // eslint-disable-next-line no-undef
   plugins: [require("tailwindcss-animate")],
 };
