9fdf6ff47edb56c3172b1904d84d2524d5a732fb
unknown <abir045@gmail.com>
2024-04-25 17:20:10 +0600

    updated server configs

---
+++ client/src/scenes/dashboard/Row1.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import DashboardBox from "@/components/DashboardBox";
+import { useGetKpisQuery } from "../state/api";
+type Props = {};
+
+const Row1 = (props: Props) => {
+  const { data } = useGetKpisQuery();
+  return (
+    <>
+      <DashboardBox gridArea="a">a</DashboardBox>
+      <DashboardBox gridArea="b">b</DashboardBox>
+      <DashboardBox gridArea="c">c</DashboardBox>
+    </>
+  );
+};
+
+export default Row1;
--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,6 +1,7 @@
 import { Box, useMediaQuery, useTheme } from "@mui/material";
-
-type Props = {};
+import Row1 from "./Row1";
+import Row2 from "./Row2";
+import Row3 from "./Row3";
 
 const gridTemplateLargeScreens = `
 " a b c "
@@ -15,7 +16,42 @@ const gridTemplateLargeScreens = `
 " g h j "
 `;
 
-const Dashboard = (props: Props) => {
+const gridTemplateSmallScreens = `
+
+"a"
+"a"
+"a"
+"a"
+"b"
+"b"
+"b"
+"b"
+"c"
+"c"
+"c"
+"d"
+"d"
+"d"
+"e"
+"e"
+"f"
+"f"
+"f"
+"g"
+"g"
+"g"
+'h" 
+'h" 
+'h" 
+'h"
+"i"
+"i"
+"j" 
+"j" 
+
+`;
+
+const Dashboard = () => {
   const isAboveMediumScreens = useMediaQuery("(min-width:1200px)");
   const { palette } = useTheme();
   return (
@@ -24,22 +60,23 @@ const Dashboard = (props: Props) => {
       height="100%"
       display="grid"
       gap="1.5rem"
-      sx={{
-        gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
-        gridTemplateRows: "repeat)3, minmax(60px, 1fr)",
-        gridTemplateAreas: gridTemplateLargeScreens,
-      }}
+      sx={
+        isAboveMediumScreens
+          ? {
+              gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+              gridTemplateRows: "repeat(3, minmax(60px, 1fr))",
+              gridTemplateAreas: gridTemplateLargeScreens,
+            }
+          : {
+              gridAutoColumns: "1fr",
+              gridAutoRows: "80px",
+              gridTemplateAreas: gridTemplateSmallScreens,
+            }
+      }
     >
-      <Box bgcolor="#fff">a</Box>
-      <Box bgcolor="#fff">b</Box>
-      <Box bgcolor="#fff">c</Box>
-      <Box bgcolor="#fff">d</Box>
-      <Box bgcolor="#fff">e</Box>
-      <Box bgcolor="#fff">f</Box>
-      <Box bgcolor="#fff">g</Box>
-      <Box bgcolor="#fff">h</Box>
-      <Box bgcolor="#fff">i</Box>
-      <Box bgcolor="#fff">j</Box>
+      <Row1 />
+      <Row2 />
+      <Row3 />
     </Box>
   );
 };
